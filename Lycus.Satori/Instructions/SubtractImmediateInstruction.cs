7fa83765edbc3e1b7ac41875a4668ecc7c178e1c
Alex Rønne Petersen <alex@lycus.org>
2013-12-30 07:45:51 +0100

    Implement decoding for ADD (IMM) and SUB (IMM).

--- Lycus.Satori/Instructions/SubtractImmediateInstruction.cs
+++ Lycus.Satori/Instructions/SubtractImmediateInstruction.cs
@@ -20,8 +20,31 @@ namespace Lycus.Satori.Instructions
         {
         }
 
+        public int SourceRegister { get; set; }
+
+        public int DestinationRegister { get; set; }
+
+        public int Immediate { get; set; }
+
+        public override void Decode()
+        {
+            SourceRegister = (int)Bits.Extract(Value, 10, 3);
+            DestinationRegister = (int)Bits.Extract(Value, 13, 3);
+            Immediate = (int)Bits.Extract(Value, 7, 3);
+
+            if (Is16Bit)
+                return;
+
+            SourceRegister |= (int)Bits.Extract(Value, 26, 3) << 3;
+            DestinationRegister = (int)Bits.Extract(Value, 29, 3) << 3;
+            Immediate |= (int)Bits.Extract(Value, 16, 8) << 8;
+        }
+
         public override Operation Execute(Core core)
         {
+            if (core == null)
+                throw new ArgumentNullException("core");
+
             return Operation.Next;
         }
     }
