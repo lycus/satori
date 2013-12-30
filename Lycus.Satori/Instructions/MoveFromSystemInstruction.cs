63a6a67ac8f7077e915de7ae84d21d04f77b1871
Alex Rønne Petersen <alex@lycus.org>
2013-12-30 08:13:27 +0100

    Implement decoding of MOVTS and MOVFS.

--- Lycus.Satori/Instructions/MoveFromSystemInstruction.cs
+++ Lycus.Satori/Instructions/MoveFromSystemInstruction.cs
@@ -20,6 +20,25 @@ namespace Lycus.Satori.Instructions
         {
         }
 
+        public int RegisterGroup { get; set; }
+
+        public int SourceRegister { get; set; }
+
+        public int DestinationRegister { get; set; }
+
+        public override void Decode()
+        {
+            SourceRegister = (int)Bits.Extract(Value, 10, 3);
+            DestinationRegister = (int)Bits.Extract(Value, 13, 3);
+
+            if (Is16Bit)
+                return;
+
+            RegisterGroup = (int)Bits.Extract(Value, 20, 2);
+            SourceRegister |= (int)Bits.Extract(Value, 26, 3) << 3;
+            DestinationRegister |= (int)Bits.Extract(Value, 29, 3) << 3;
+        }
+
         public override Operation Execute(Core core)
         {
             return Operation.Next;
