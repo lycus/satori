0039c2a58a6874f47a0bfe56855a70e576109c8d
Alex Rønne Petersen <alex@lycus.org>
2014-01-02 00:27:59 +0100

    Implement decoding of all forms of LDR/STR.

--- Lycus.Satori/Instructions/LoadPostModifyInstruction.cs
+++ Lycus.Satori/Instructions/LoadPostModifyInstruction.cs
@@ -20,8 +20,33 @@ namespace Lycus.Satori.Instructions
         {
         }
 
+        public int Size { get; set; }
+
+        public bool Subtract { get; set; }
+
+        public int SourceRegister { get; set; }
+
+        public int OperandRegister { get; set; }
+
+        public int DestinationRegister { get; set; }
+
+        public override void Decode()
+        {
+            SourceRegister = (int)Bits.Extract(Value, 10, 3) | (int)Bits.Extract(Value, 26, 3) << 3;
+            OperandRegister = (int)Bits.Extract(Value, 7, 3) | (int)Bits.Extract(Value, 23, 3) << 3;
+            DestinationRegister = (int)Bits.Extract(Value, 13, 3) | (int)Bits.Extract(Value, 29, 3) << 3;
+            Size = (int)Bits.Extract(Value, 5, 2);
+            Subtract = Bits.Check(Value, 20);
+
+            if (Size == 0x3 && SourceRegister % 2 != 0)
+                throw InstructionException();
+        }
+
         public override Operation Execute(Core core)
         {
+            if (core == null)
+                throw new ArgumentNullException("core");
+
             return Operation.Next;
         }
     }
--- Lycus.Satori/Instructions/StoreDisplacementPostModifyInstruction.cs
+++ Lycus.Satori/Instructions/StoreDisplacementPostModifyInstruction.cs
@@ -20,8 +20,41 @@ namespace Lycus.Satori.Instructions
         {
         }
 
+        public int Size { get; set; }
+
+        [CLSCompliant(false)]
+        public uint Immediate { get; set; }
+
+        public bool Subtract { get; set; }
+
+        public int SourceRegister { get; set; }
+
+        public int DestinationRegister { get; set; }
+
+        public override void Decode()
+        {
+            SourceRegister = (int)Bits.Extract(Value, 13, 3);
+            DestinationRegister = (int)Bits.Extract(Value, 10, 3);
+            Immediate = Bits.Extract(Value, 7, 3);
+            Size = (int)Bits.Extract(Value, 5, 2);
+
+            if (Is16Bit)
+                return;
+
+            SourceRegister |= (int)Bits.Extract(Value, 29, 3) << 3;
+            DestinationRegister |= (int)Bits.Extract(Value, 26, 3) << 3;
+            Immediate |= Bits.Extract(Value, 16, 8) << 8;
+            Subtract = Bits.Check(Value, 24);
+
+            if (Size == 0x3 && DestinationRegister % 2 != 0)
+                throw InstructionException();
+        }
+
         public override Operation Execute(Core core)
         {
+            if (core == null)
+                throw new ArgumentNullException("core");
+
             return Operation.Next;
         }
     }
