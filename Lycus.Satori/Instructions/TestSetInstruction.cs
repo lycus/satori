d2e4293c13a454309ce5f8c251d3a1e3ec5b1d10
Alex Rønne Petersen <alex@lycus.org>
2014-01-02 03:58:08 +0100

    Implement TESTSET logic.

--- Lycus.Satori/Instructions/TestSetInstruction.cs
+++ Lycus.Satori/Instructions/TestSetInstruction.cs
@@ -20,8 +20,6 @@ namespace Lycus.Satori.Instructions
         {
         }
 
-        public int Size { get; set; }
-
         public bool Subtract { get; set; }
 
         public int SourceRegister { get; set; }
@@ -35,17 +33,41 @@ namespace Lycus.Satori.Instructions
             SourceRegister = (int)Bits.Extract(Value, 13, 3) | (int)Bits.Extract(Value, 29, 3) << 3;
             OperandRegister = (int)Bits.Extract(Value, 7, 3) | (int)Bits.Extract(Value, 23, 3) << 3;
             DestinationRegister = (int)Bits.Extract(Value, 10, 3) | (int)Bits.Extract(Value, 26, 3) << 3;
-            Size = (int)Bits.Extract(Value, 5, 2);
             Subtract = Bits.Check(Value, 20);
-
-            if (Size == 0x3 && SourceRegister % 2 != 0)
-                throw InstructionException();
         }
 
         public override Operation Execute(Core core)
         {
             if (core == null)
                 throw new ArgumentNullException("core");
+
+            var src1 = (uint)core.Registers[SourceRegister];
+            var src2 = (uint)core.Registers[OperandRegister];
+            var addr = Subtract ? src1 - src2 : src1 + src2;
+
+            Core tgt;
+            byte[] bank;
+            object @lock;
+
+            core.Machine.Memory.Translate(core, addr,
+                out tgt, out bank, out @lock);
+
+            if (tgt == null)
+                throw new MemoryException(
+                    "Attempted external TESTSET at 0x{0:X8}.".Interpolate(addr),
+                    addr, false);
+
+            int value;
+
+            lock (@lock)
+            {
+                value = core.Machine.Memory.ReadInt32(core, addr);
+
+                if (value != 0)
+                    core.Machine.Memory.Write(core, addr, core.Registers[DestinationRegister]);
+            }
+
+            core.Registers[DestinationRegister] = value;
 
             return Operation.Next;
         }
