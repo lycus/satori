0fb22cfdb1ecd60c8cf946c7a9784c76fb6c34ce
Alex Rønne Petersen <alex@lycus.org>
2014-01-06 07:00:25 +0100

    Always decode an Rm operand in FIX, FABS, and FLOAT.
    This must equal the Rn operand.

--- Lycus.Satori/Instructions/FloatInstruction.cs
+++ Lycus.Satori/Instructions/FloatInstruction.cs
@@ -22,18 +22,27 @@ namespace Lycus.Satori.Instructions
 
         public int SourceRegister { get; set; }
 
+        public int OperandRegister { get; set; }
+
         public int DestinationRegister { get; set; }
 
         public override void Decode()
         {
             SourceRegister = (int)Bits.Extract(Value, 10, 3);
+            OperandRegister = (int)Bits.Extract(Value, 7, 3);
             DestinationRegister = (int)Bits.Extract(Value, 13, 3);
 
             if (Is16Bit)
                 return;
 
             SourceRegister |= (int)Bits.Extract(Value, 26, 3) << 3;
+            OperandRegister |= (int)Bits.Extract(Value, 23, 3) << 3;
             DestinationRegister = (int)Bits.Extract(Value, 29, 3) << 3;
+
+            // The `Rm` field isn't actually used for anything, but must
+            // equal the `Rn` field.
+            if (OperandRegister != SourceRegister)
+                throw InstructionException();
         }
 
         public override Operation Execute(Core core)
